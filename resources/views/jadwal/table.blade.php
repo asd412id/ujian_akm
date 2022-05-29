44b8cc46eebe430a46607601b3c08a7b3d02a930
asd412id <asdar.binsyam@gmail.com>
2022-05-29 23:32:33 +0800

    Fix jadwal table whitespace

--- resources/views/jadwal/table.blade.php
+++ resources/views/jadwal/table.blade.php
@@ -1,51 +1,51 @@
-<div class="bg-white shadow-md rounded">
-  <table class="text-left w-full border-collapse">
+<div class="bg-white rounded shadow-md">
+  <table class="w-full text-left border-collapse">
     <thead>
       <tr>
-        <th class="py-4 px-6 bg-gray-100 font-bold uppercase text-sm text-gray-700 border-b border-gray-100">
+        <th class="px-6 py-4 text-sm font-bold text-gray-700 uppercase bg-gray-100 border-b border-gray-100">
           Nama</th>
-        <th class="py-4 px-6 bg-gray-100 font-bold uppercase text-sm text-gray-700 border-b border-gray-100">
+        <th class="px-6 py-4 text-sm font-bold text-gray-700 uppercase bg-gray-100 border-b border-gray-100">
           Ruangan</th>
-        <th class="py-4 px-6 bg-gray-100 font-bold uppercase text-sm text-gray-700 border-b border-gray-100">
+        <th class="px-6 py-4 text-sm font-bold text-gray-700 uppercase bg-gray-100 border-b border-gray-100">
           Waktu</th>
-        <th class="py-4 px-6 bg-gray-100 font-bold uppercase text-sm text-gray-700 border-b border-gray-100">
+        <th class="px-6 py-4 text-sm font-bold text-gray-700 uppercase bg-gray-100 border-b border-gray-100">
           Durasi</th>
-        <th class="py-4 px-6 bg-gray-100 font-bold uppercase text-sm text-gray-700 border-b border-gray-100">
+        <th class="px-6 py-4 text-sm font-bold text-gray-700 uppercase bg-gray-100 border-b border-gray-100">
           Status</th>
-        <th class="text-right py-4 px-6 bg-gray-100 font-bold uppercase text-sm text-gray-700 border-b border-gray-100">
+        <th class="px-6 py-4 text-sm font-bold text-right text-gray-700 uppercase bg-gray-100 border-b border-gray-100">
           Aksi</th>
       </tr>
     </thead>
     <tbody>
       @forelse ($data as $key => $v)
       <tr class="{{ !$v->active && $v->logins()->count() ? 'bg-primary-100' : 'hover:bg-gray-100' }}">
-        <td class="py-4 px-6 border-b border-gray-100">
+        <td class="px-6 py-4 border-b border-gray-100 whitespace-nowrap">
           <div class="flex flex-col">
             <span>{{ $v->name }}</span>
             <em class="-mt-1 text-sm text-gray-500">{!! nl2br($v->desc) !!}</em>
           </div>
         </td>
-        <td class="py-4 px-6 border-b border-gray-100"><span
-            class="text-sm bg-lime-50 border border-lime-200 shadow-md text-lime-700 py-1 px-3 rounded-lg">{!!
+        <td class="px-6 py-4 border-b border-gray-100 whitespace-nowrap"><span
+            class="px-3 py-1 text-sm border rounded-lg shadow-md bg-lime-50 border-lime-200 text-lime-700">{!!
             implode('</span> <span
-            class="text-sm bg-lime-50 border border-lime-200 shadow-md text-lime-700 py-1 px-3 rounded-lg">
+            class="px-3 py-1 text-sm border rounded-lg shadow-md bg-lime-50 border-lime-200 text-lime-700">
             ',array_unique($v->pesertas()->select('ruang')->distinct('ruang')->get()->pluck('ruang')->toArray()))
             !!}</span></td>
-        <td class="py-4 px-6 border-b border-gray-100">
-          <span class="text-sm bg-amber-50 border border-amber-200 shadow-md text-amber-700 py-1 px-3 rounded-lg">{{
+        <td class="px-6 py-4 border-b border-gray-100 whitespace-nowrap">
+          <span class="px-3 py-1 text-sm border rounded-lg shadow-md bg-amber-50 border-amber-200 text-amber-700">{{
             $v->start->format('d/m/Y H:i')
             }} - {{
             $v->end->format('d/m/Y H:i') }}</span>
         </td>
-        <td class="py-4 px-6 border-b border-gray-100">
-          <span class="text-sm bg-sky-50 border border-sky-200 shadow-md text-sky-700 py-1 px-3 rounded-lg">{{
+        <td class="px-6 py-4 border-b border-gray-100 whitespace-nowrap">
+          <span class="px-3 py-1 text-sm border rounded-lg shadow-md bg-sky-50 border-sky-200 text-sky-700">{{
             $v->duration.' Menit' }}</span>
         </td>
-        <td class="py-4 px-6 border-b border-gray-100">{!! $v->active?'<span
-            class="text-sm bg-positive-50 text-positive-700 border border-positive-200 shadow-md py-1 px-3 rounded-lg">Aktif</span>':'<span
-            class="text-sm bg-negative-50 text-negative-700 border border-negative-200 shadow-md py-1 px-3 rounded-lg">Tidak
+        <td class="px-6 py-4 border-b border-gray-100 whitespace-nowrap">{!! $v->active?'<span
+            class="px-3 py-1 text-sm border rounded-lg shadow-md bg-positive-50 text-positive-700 border-positive-200">Aktif</span>':'<span
+            class="px-3 py-1 text-sm border rounded-lg shadow-md bg-negative-50 text-negative-700 border-negative-200">Tidak
             Aktif</span>' !!}</td>
-        <td class="py-4 px-6 border-b border-gray-100">
+        <td class="px-6 py-4 border-b border-gray-100 whitespace-nowrap">
           <div class="flex justify-end">
             <div class="flex flex-wrap justify-end gap-1">
               @if ((!$v->active && !$v->logins()->count()) || $v->active)
@@ -72,7 +72,7 @@
       </tr>
       @empty
       <tr class="hover:bg-gray-100">
-        <td colspan="5" class="py-4 px-6 border-b border-gray-100 text-center">Data tidak tersedia!</td>
+        <td colspan="5" class="px-6 py-4 text-center border-b border-gray-100">Data tidak tersedia!</td>
       </tr>
       @endforelse
     </tbody>
