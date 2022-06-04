4a4c7b47a0320cf84475c3741a1a224d6848de41
asd412id <asdar.binsyam@gmail.com>
2022-06-05 02:39:41 +0800

    Add limit uploads filemanager

--- app/filemanager.php
+++ app/filemanager.php
@@ -1,10 +1,9 @@
 <?php
 return [
-  'MaxSizeTotal' => 150,
+  'MaxSizeTotal' => 100,
   'MaxSizeUpload' => 10,
   'show_total_size' => true,
   'show_folder_size' => true,
-  'default_language' => "id_ID",
   'convert_spaces' => true,
   'replace_with' => "_",
   'lower_case' => true,
