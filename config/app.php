b2a1dbd069894d32bc4f2293e04eee8e0fa059c0
asd412id <asdar.binsyam@gmail.com>
2022-06-11 13:26:13 +0800

    Add minor update version

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
-    'version' => env('APP_VERSION', '1.0'),
+    'version' => env('APP_VERSION', '1.0.0'),
 
     /*
     |--------------------------------------------------------------------------
